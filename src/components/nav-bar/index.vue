541bcc99a06f976c6f1d5917198b1ee2b5f8678c
liqin <1483510623@qq.com>
2018-07-17 11:11:38 +0800

    编程式导航

--- src/components/nav-bar/index.vue
+++ src/components/nav-bar/index.vue
@@ -1,26 +1,30 @@
 <template>
     <div>
         <mt-navbar v-model="selected">
-            <mt-tab-item :id="index" v-for="(item,index) in routes" :key="item.path">
-                   <router-link :to="item.path">
-                        {{item.title}}
-                    </router-link>
+            <mt-tab-item :id="index" 
+                @click.native="linkFn(item.path)" 
+                v-for="(item,index) in routes" 
+                :key="item.path">
+                 {{item.title}}
             </mt-tab-item>
         </mt-navbar>
     </div>
 </template>
 <script>
 import {routes} from "@/router/routes.js"
-//使用路由导航的形式进行路由的跳转 :绑定动态的属性 @绑定事件 v-model:select绑定元素的id 
+//使用路由导航的形式进行路由的跳转 :绑定动态的属性 @绑定事件 v-model:select绑定元素的id
+// 在插件中加载原生的事件native  
 export default {
     data(){
         return {
             selected:0,
-            routes,
+            routes,//initdata create mounted updata destory
         }
     },
     methods:{
-
+        linkFn(path){//使用编程式路由，进行路由的跳转
+           this.$router.push({ path})
+        }
     }
 }
 </script>
