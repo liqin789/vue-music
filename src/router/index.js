a578f3e11f2a930edc325d8b2283d3781b8d42b5
liqin <1483510623@qq.com>
2018-07-18 11:35:18 +0800

    鼠标的滚动行为

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,14 @@ Vue.use(Router)
 
 export default new Router({
   mode:'history',
+  scrollBehavior (to, from, savedPosition) {
+    // return 期望滚动到哪个的位置 元素的滚动的行为
+    if(savedPosition){
+      return savedPosition
+    }else{
+      return {x:0,y:0}
+    }
+  },
   routes: [
      ...routes,
     {
