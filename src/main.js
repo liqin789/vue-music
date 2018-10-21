ba0d582d109a23f26b91edaff0c930518517ed95
liqin <1483510623@qq.com>
2018-10-21 14:03:25 +0800

    数据滚动加载更多数据

--- src/main.js
+++ src/main.js
@@ -15,6 +15,13 @@ Vue.prototype.$http = axios
 import 'mint-ui/lib/style.css'; 
 Vue.use(Mint)
 
+//引入element-ui
+import Element from 'element-ui';
+
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(Element)
+
+
 
 import 'vue-tree-halower/dist/halower-tree.min.css' // you can customize the style of the tree
 import VTree from 'vue-tree-halower'
---
+++ src/views/plan/addPlan.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+          <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="planRuleForm">
+              
+          </el-form>
+
+
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+ 
+</style>
+
